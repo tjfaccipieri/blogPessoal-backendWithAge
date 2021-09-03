35be2a0dfd230b64db66bff8fe40fa84829a1217
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2021-09-03 15:07:21 -0300

    versão

--- src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -38,7 +38,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
-			.antMatchers("/usuarios").permitAll()
+			.antMatchers("/usuarios/**").permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
