797807424d5c86875f1df35a11ead7226575289d
Thiago <thiago.faccipieri@gmail.com>
2021-09-25 09:10:46 -0300

    ajustado security - HttpMethod.GET

--- src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -39,7 +39,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
-			
+			.antMatchers(HttpMethod.GET, "/postagens/**").permitAll()
+			.antMatchers(HttpMethod.GET, "/temas/**").permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
